f74f9dfaa4f227f86f1ea6ff7b5c322d58fbf1b2
Pedro <pedrohk@gmail.com>
2025-06-20 01:43:38 -0300

    mocked tests

--- src/test/java/org/service/order/UserServiceTest.java
+++ src/test/java/org/service/order/UserServiceTest.java
@@ -2,6 +2,9 @@ package org.service.order;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -10,9 +13,12 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
@@ -44,5 +50,37 @@ public class UserServiceTest {
         verify(userRepository, times(1)).findByEmail("mariana.ribeiro89@gmail.com");
         verify(userRepository, times(1)).save(newUser);
         verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    void testCreateUserEmailExists() {
+        User existingUser = new User("1", "Camila Mendes Rocha", "camila.rocha07@gmail.net");
+        User newUser = new User(null, "Nova Camila", "camila.rocha07@gmail.net");
+
+        when(userRepository.findByEmail("camila.rocha07@gmail.net")).thenReturn(Optional.of(existingUser));
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            userService.createUser(newUser);
+        });
+
+        assertEquals("User with email camila.rocha07@gmail.net already exists.", exception.getMessage());
+
+        verify(userRepository, times(1)).findByEmail("camila.rocha07@gmail.net");
+        verify(userRepository, never()).save(any(User.class)); // Ensure save was NOT called
+        verifyNoMoreInteractions(userRepository);
+    }
+
+    @ParameterizedTest(name = "Should throw IllegalArgumentException for null/empty user/email: {0}")
+    @NullAndEmptySource
+    @ValueSource(strings = {"   ", "\t"})
+    void testCreateUserInvalidInput(String email) {
+        User invalidUser = new User(null, "Camila Mendes Rocha", email);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            userService.createUser(invalidUser);
+        });
+
+        assertEquals("User and email cannot be null or empty.", exception.getMessage());
+        verifyNoInteractions(userRepository);
     }
 }
